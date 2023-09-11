b3402545713a43a0fcd497386a1e0f57329b181e
r-swrt-GB <rikus@greenbit.dev>
2023-09-11 09:25:52 +0200

    dicee-flutter

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
 ![App Brewery Banner](https://github.com/londonappbrewery/Images/blob/master/AppBreweryBanner.png)
 
-# Mi Card
+
+# Dicee 🎲
 
 ## Our Goal
 
-Now that you've seen how to create a Flutter app entirely from scratch, we're going to go further and learn more about how to design user interfaces for Flutter apps.
+The objective of this tutorial is to introduce you to the core programming concepts that will form the foundation of most of the apps you’ll build in the future. This app will teach you how to make apps with functionality using setState() inside Stateful Flutter widgets.
+
 
 ## What you will create
 
-Mi Card is a personal business card. Imagine every time you wanted to give someone your contact details or your business card but you didn't have it on you. Well, now you can get them to download your business card as an app.
+We’re going to make a Las Vegas dice app. You can make the die roll at the press of a button. With this app in your pocket, you’ll be able to settle any score on the go!
+
+![Finished App](https://github.com/londonappbrewery/Images/blob/master/dicee-demo.gif)
 
 ## What you will learn
 
-* How to create Stateless Widgets
-* What is the difference between hot reload and hot refresh and running an app from cold
-* How to use Containers to lay out your UI
-* How to use Columns and Rows to position your UI elements
-* How to add custom fonts
-* How to add Material icons
-* How to style Text widgets
-* How to read and use Flutter Documentation
-
-
+- How to use Flutter stateless widgets to design the user interface.
+- How to use Flutter stateful widgets to update the user interface.
+- How to change the properties of various widgets.
+- How to use onPressed listeners to detect when buttons are pressed.
+- How to use setState to mark the widget tree as dirty and requiring update on the next render.
+- How to use Expanded to make widgets adapt to screen dimensions.
+- Understand and use string interpolation.
+- Learn about basic dart programming concepts such as data types and functions.
+- Code and use gesture controls.
 
 >This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,59 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyAppp(),
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
   );
 }
 
-class MyAppp extends StatelessWidget {
-  const MyAppp({Key key}) : super(key: key);
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1, rightDice = 1;
+  void updateDice() async {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage("images/profilePic.png"),
-                radius: 50.0,
-              ),
-              Text(
-                "Rikus Swart",
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Pacifico",
-                ),
-              ),
-              Text(
-                "FLUTTER DEVELOPER",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontFamily: "Source Sans Pro",
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[500],
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.grey,
-                ),
-              ),
-              Card(
-                color: Colors.black,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.white,
-                  ),
-                  title: Text(
-                    "+27 60 504 5379",
-                    style: TextStyle(
-                      fontFamily: "Source Sans Pro",
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                color: Colors.black,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.white,
-                  ),
-                  title: Text(
-                    "Rikusswart101@gmail.com",
-                    style: TextStyle(
-                      fontFamily: "Source Sans Pro",
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                updateDice();
+              },
+              child: Image.asset("images/dice$leftDice.png"),
+            ),
           ),
-        ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                updateDice();
+              },
+              child: Image.asset("images/dice$rightDice.png"),
+            ),
+          ),
+        ],
       ),
     );
   }
