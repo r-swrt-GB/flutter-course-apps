0b70657016ba7b81f18c345c12d796ca981e98c9
r-swrt-GB <rikus@greenbit.dev>
2023-09-11 09:23:38 +0200

    i_am_rich

--- android/app/src/main/kotlin/com/example/i_am_rich/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/i_am_rich/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.i_am_poor
+package com.example.i_am_rich
 
 import io.flutter.embedding.android.FlutterActivity
 
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"i_am_poor", origin, size)) {
+  if (!window.CreateAndShow(L"i_am_rich", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
